0c9ad94da390d09a5b31e2a3d5fb73301559aae0
anuragdubey2004 <anuragdubey635@gmail.com>
2025-12-25 17:29:26 +0530

    update

--- 01-Folder/src/App.jsx
+++ 01-Folder/src/App.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 const App = () => {
   return (
-    <h1>Hellow Worlds</h1>
-  )
+    <> 
+      <h1>Hellow Worlds</h1>
+      <h2> Hello</h2>
+    </>
+  ) 
 }
 
 export default App
