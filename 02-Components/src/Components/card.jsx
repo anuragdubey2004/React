f6ecdd9a7f5a1fd09c5e886fcf0b8ac64fa8644b
anuragdubey2004 <anuragdubey635@gmail.com>
2025-12-25 23:13:35 +0530

    updated

---
+++ 02-Components/src/Components/card.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className = "card">
+        <h2>Hello im card</h2>
+        <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+            Ullam animi explicabo praesentium. Error eaque sed vitae, consectetur, 
+            voluptates facilis quod iure corrupti soluta esse quam ipsa impedit deleniti quae quia!
+        </p>
+    </div>
+  )
+}
+
+export default Card
