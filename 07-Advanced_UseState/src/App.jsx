1659fa95120a8e2d5d032b04c6cb5eb96fac713e
anuragdubey2004 <anuragdubey635@gmail.com>
2025-12-31 15:53:31 +0530

    Completed

--- 07-Advanced_UseState/src/App.jsx
+++ 07-Advanced_UseState/src/App.jsx
@@ -1,34 +1,20 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
 
+  const [num, setnum] = useState({user:'Anurag', age:21});
+
+  const changed = () => {
+    const newnum = {...num}; // {} -> obj and []-> array
+    newnum.user = 'Jack';
+    newnum.age = 32;
+    setnum(newnum);
+  }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>{num.user}, {num.age}</h1>
+      <button onClick={changed} className='btn'>Click Me</button>
+    </div>
   )
 }
 
