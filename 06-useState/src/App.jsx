539d193161d650fad41f163199ba80b10a7c93e1
anuragdubey2004 <anuragdubey635@gmail.com>
2025-12-31 15:06:00 +0530

    Completed

--- 06-useState/src/App.jsx
+++ 06-useState/src/App.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { useState } from 'react'
 
 const App = () => {
+
+  const [num, setNum] = useState(0);
+
+  const increaseN = () =>{
+    setNum(num+1); // we cannot use num++
+  }
+
+  const decreaseN = () => {
+    setNum(num-1);
+  }
+
   return (
-    <div>
-      
+    <div className='container'>
+      <div className="box"><h1>{num}</h1></div> 
+      <div className="btn">
+         <button onClick={increaseN}>Increase</button>
+         <button onClick={decreaseN}>Decrease</button>
+      </div>
     </div>
   )
 }
