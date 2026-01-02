8e8d58c29e6d8532ff989973e109da73a3fade5c
anuragdubey2004 <anuragdubey635@gmail.com>
2026-01-02 10:09:40 +0530

    completed

---
+++ 10-Local-Storage/src/App.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const App = () => {
+
+  //  localStorage.clear();  // clear the local storage
+  //  sessionStorage.clear();
+
+  //   localStorage.setItem('user', 'pair');  //set data
+  //   localStorage.setItem('age', '18');
+  //   const user = localStorage.getItem('user');  //get data
+  //   const ageUser = localStorage.getItem('age');
+
+  //   localStorage.removeItem('user'); //remove data
+
+  //   console.log(user, ageUser);
+
+  // const user = {
+  //   username : 'Anurag',
+  //   age : 21,
+  //   city : 'Ayodhya'
+  // }
+
+  // localStorage.setItem('user', user);
+  // localStorage.setItem('userObj', JSON.stringify(user));
+
+    
+
+  return (
+    <div>
+      <div>App</div>
+    </div>
+  )
+}
+
+export default App
